67418b763abf34f769c3edd531dedabf831f70f3
Brett E Stockmeier <stockmeierb@gmail.com>
2019-08-08 22:01:06 -0500

    display custom component

--- App.js
+++ App.js
@@ -1,14 +1,16 @@
-import { createStackNavigator, createAppContainer } from 'react-navigation';
-import HomeScreen from './src/screens/HomeScreen';
+import { createStackNavigator, createAppContainer } from "react-navigation";
+import HomeScreen from "./src/screens/HomeScreen";
+import ComponentScreen from "./src/screens/ComponentsScreen";
 
 const navigator = createStackNavigator(
   {
-    Home: HomeScreen
+    Home: HomeScreen,
+    Components: ComponentScreen
   },
   {
-    initialRouteName: 'Home',
+    initialRouteName: "Components",
     defaultNavigationOptions: {
-      title: 'App'
+      title: "App"
     }
   }
 );
