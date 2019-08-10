9d3e3a094c551849041fafc60e1f3cfcdf9b8dc2
Brett E Stockmeier <stockmeierb@gmail.com>
2019-08-09 22:43:41 -0500

    JSX exercise solution

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -2,9 +2,12 @@ import React from "react";
 import { Text, StyleSheet } from "react-native";
 
 const ComponentScreen = () => {
+  const Name = "Brett Stockmeier";
   return (
     <View>
       <Text style={styles.textStyle}>Hello World</Text>
+      <Text style={styles.textStyle1}>Getting Started with React Native!</Text>
+      <Text style={styles.textStyle2}>My Name is {Name}!</Text>
     </View>
   );
 };
@@ -12,6 +15,12 @@ const ComponentScreen = () => {
 const styles = StyleSheet.create({
   textStyle: {
     fontSize: 30
+  },
+  textStyle1: {
+    fontSize: 45
+  },
+  textStyle2: {
+    fontSize: 20
   }
 });
 
