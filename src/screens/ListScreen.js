61d4a7e05eaf2e6d213f1fe433ac788e91c8d546
Brett E Stockmeier <stockmeierb@gmail.com>
2019-08-10 21:27:16 -0500

    Create new List Screen component

---
+++ src/screens/ListScreen.js
@@ -0,0 +1,10 @@
+import React from "React";
+import { View, Text, StyleSheet } from "react-native";
+
+const ListScreen = () => {
+  return (
+    <View>
+      <Text>Hello World</Text>
+    </View>
+  );
+};
