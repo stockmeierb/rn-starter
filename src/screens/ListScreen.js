1f8c0fa3f853975dddf9f36325a97d075a028cb1
Brett E Stockmeier <stockmeierb@gmail.com>
2019-08-11 08:34:46 -0500

    add Flatlist w/o key

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -1,10 +1,31 @@
 import React from "React";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, FlatList } from "react-native";
 
 const ListScreen = () => {
+  const friends = [
+    { name: "friend0" },
+    { name: "friend1" },
+    { name: "friend2" },
+    { name: "friend3" },
+    { name: "friend4" },
+    { name: "friend5" },
+    { name: "friend6" },
+    { name: "friend7" },
+    { name: "friend8" },
+    { name: "friend9" }
+  ];
   return (
     <View>
-      <Text>Hello World</Text>
+      <FlatList
+        data={friends}
+        renderItem={({ item }) => {
+          return (
+            <View>
+              <Text>{item.name}</Text>
+            </View>
+          );
+        }}
+      />
     </View>
   );
 };
