22983ffddfbda873f7d2a45ade98d26a7bb7e76b
Brett E Stockmeier <stockmeierb@gmail.com>
2019-08-13 22:18:13 -0500

    buttons!

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,13 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View, Button } from "react-native";
 
 const HomeScreen = () => {
-  return <Text style={styles.text}>Hello World.</Text>;
+  return (
+    <View>
+      <Text style={styles.text}>Hello World.</Text>
+      <Button />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
