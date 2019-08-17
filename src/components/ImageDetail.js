821a9a9e589e893739bc4205a4f9730648d32bb6
Brett E Stockmeier <stockmeierb@gmail.com>
2019-08-17 17:39:13 -0500

    image detail boilerplate

---
+++ src/components/ImageDetail.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const imageDetail = () => {
+  return (
+    <View>
+      <Text>Hello World</Text>
+    </View>
+  );
+};
+
+const styles = new StyleSheet.create({});
+
+export default imageDetail;
