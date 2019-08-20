af233c8dbc7250ee65c86edc3c1d68d35390a8f1
Brett E Stockmeier <stockmeierb@gmail.com>
2019-08-19 23:13:05 -0500

    hardcoding props

--- src/screens/ImageScreen.js
+++ src/screens/ImageScreen.js
@@ -6,9 +6,18 @@ const ImageScreen = () => {
   return (
     <View>
       <Text>Image Screen Component</Text>
-      <ImageDetail title="forest" />
-      <ImageDetail title="beach" />
-      <ImageDetail title="mountain" />
+      <ImageDetail
+        title="forest"
+        imageSource={require("../../assets/forest.jpg")}
+      />
+      <ImageDetail
+        title="beach"
+        imageSource={require("../../assets/beach.jpg")}
+      />
+      <ImageDetail
+        title="mountain"
+        imageSource={require("../../assets/mountain.jpg")}
+      />
     </View>
   );
 };
