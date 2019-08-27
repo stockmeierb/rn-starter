ea964cfafff8d4dfaf168855b920d36c6305ea40
Brett E Stockmeier <stockmeierb@gmail.com>
2019-08-26 21:34:37 -0500

    new component boilerplate ColorScreen

---
+++ src/screens/ColorScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, StyleSheet, Button } from "react-native";
+
+const ColorScreen = () => {
+  return (
+    <View>
+      <Button />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default ColorScreen;
