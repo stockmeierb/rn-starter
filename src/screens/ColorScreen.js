51cc8dd3014233ff358fce5073b85f6b2fa56b57
Brett E Stockmeier <stockmeierb@gmail.com>
2019-08-27 21:55:54 -0500

    implement random rgb function

--- src/screens/ColorScreen.js
+++ src/screens/ColorScreen.js
@@ -5,8 +5,17 @@ const ColorScreen = () => {
   return (
     <View>
       <Button />
+      <View style={{ height: 100, width: 100, backgroundColor: randomRgb() }} />
     </View>
   );
+};
+
+const randomRgb = () => {
+  const red = Math.floor(Math.random(0, 256));
+  const green = Math.floor(Math.random(0, 256));
+  const blue = Math.floor(Math.random(0, 256));
+
+  return `rgb(${red},${green},${blue})`;
 };
 
 const styles = StyleSheet.create({});
