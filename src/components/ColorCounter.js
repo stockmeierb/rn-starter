400c973ddd8dcc85490fc8e42021ab3576b47aa7
Brett E Stockmeier <stockmeierb@gmail.com>
2019-09-02 15:13:40 -0500

    New reusable color counter component w/ boilerplate code

---
+++ src/components/ColorCounter.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const ColorCounter = () => {
+  return (
+    <View>
+      <Text>Hello World</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default ColorCounter;
