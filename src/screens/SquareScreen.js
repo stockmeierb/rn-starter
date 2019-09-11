ed9d06f08bedddcbf238a13985133d582411b7aa
Brett E Stockmeier <stockmeierb@gmail.com>
2019-09-10 22:32:51 -0500

    reducer half done

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -1,10 +1,27 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import ColorCounter from "../components/ColorCounter";
 
 const COLOR_INCREMENT = 15;
 
+const reducer = (state, action) => {
+  //action == {color, ammount}
+
+  switch (action.color) {
+    case "red":
+      return;
+    case "blue":
+      return;
+    case "green":
+      return;
+    default:
+      return;
+  }
+};
+
 const SquareScreen = () => {
+  const [state, dispatch] = useReducer(reducer, { red: 0, green: 0, blue: 0 });
+
   return (
     <View>
       <ColorCounter onIncrease={() => {}} onDecrease={() => {}} color="Red" />
