53eb5776e8794fd7adc573ebc8ad6211d386a3d2
Brett E Stockmeier <stockmeierb@gmail.com>
2019-10-03 22:16:26 -0500

    box model styles playing around

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -3,12 +3,15 @@ import { Text, View, StyleSheet } from "react-native";
 
 const BoxScreen = () => {
   return (
-    <View>
-      <Text>Box Screen</Text>
+    <View style={styles.viewStyle}>
+      <Text style={styles.textStyle}>Box Screen</Text>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  viewStyle: { borderWidth: 3, borderColor: "black" },
+  textStyle: { borderWidth: 1, borderColor: "red" }
+});
 
 export default BoxScreen;
