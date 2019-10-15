d73d73ae486a7b77f75a31b0b0f680359d6c2bcf
Brett E Stockmeier <stockmeierb@gmail.com>
2019-10-14 21:46:37 -0500

    some flex practice

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -4,14 +4,16 @@ import { Text, View, StyleSheet } from "react-native";
 const BoxScreen = () => {
   return (
     <View style={styles.viewStyle}>
-      <Text style={styles.textStyle}>Box Screen</Text>
+      <Text style={styles.textStyle}>Child No 1</Text>
+      <Text style={styles.textStyle}>Child No 2</Text>
+      <Text style={styles.textStyle}>Child No 3</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  viewStyle: { borderWidth: 3, borderColor: "black" },
-  textStyle: { borderWidth: 1, borderColor: "red" }
+  viewStyle: { borderWidth: 3, borderColor: "black", alignItems: "flex-end" },
+  textStyle: { borderWidth: 3, borderColor: "red" }
 });
 
 export default BoxScreen;
